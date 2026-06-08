e33c6651ee80b638bba0a84812a631857879f05a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-09 04:04:10 +0800

    add biological rules

--- src/EvoriumEra/Cell.vb
+++ src/EvoriumEra/Cell.vb
@@ -14,7 +14,9 @@
 End Class
 
 Public Class Voxel
-    Public Property X, Y, Z As Integer
+    Public Property X As Integer
+    Public Property Y As Integer
+    Public Property Z As Integer
     Public Property ExternalMolecules As Dictionary(Of MoleculeType, Integer) = New Dictionary(Of MoleculeType, Integer)
     Public Property Occupant As Cell = Nothing
     Public Property HasBiofilm As Boolean = False
