428552b9422d5df1c7154c5b0b63aabb7c2fb8a2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-09 04:14:29 +0800

    biological rules

---
+++ src/EvoriumEra/BiologicalRules/AerobicEnergyMetabolismRule.vb
@@ -0,0 +1,16 @@
+﻿Public Class AerobicEnergyMetabolismRule : Implements IBiochemicalRule
+    Public Sub Execute(cell As Cell, env As Environment3D, rng As Random) Implements IBiochemicalRule.Execute
+        If cell.InternalMolecules.ContainsKey(MoleculeType.Glucose) AndAlso
+           cell.InternalMolecules.ContainsKey(MoleculeType.Oxygen) Then
+
+            If cell.InternalMolecules(MoleculeType.Glucose) > 0 AndAlso
+               cell.InternalMolecules(MoleculeType.Oxygen) > 0 Then
+
+                cell.InternalMolecules(MoleculeType.Glucose) -= 1
+                cell.InternalMolecules(MoleculeType.Oxygen) -= 1
+                cell.ATP = Math.Min(cell.ATP + 12, 1000)
+                AddMolecule(cell, MoleculeType.CarbonDioxide, 6)
+            End If
+        End If
+    End Sub
+End Class
--- src/EvoriumEra/BiologicalRules/RuleScheduler.vb
+++ src/EvoriumEra/BiologicalRules/RuleScheduler.vb
@@ -2,22 +2,7 @@
     Sub Execute(cell As Cell, env As Environment3D, rng As Random)
 End Interface
 
-Public Class AerobicEnergyMetabolismRule : Implements IBiochemicalRule
-    Public Sub Execute(cell As Cell, env As Environment3D, rng As Random) Implements IBiochemicalRule.Execute
-        If cell.InternalMolecules.ContainsKey(MoleculeType.Glucose) AndAlso
-           cell.InternalMolecules.ContainsKey(MoleculeType.Oxygen) Then
 
-            If cell.InternalMolecules(MoleculeType.Glucose) > 0 AndAlso
-               cell.InternalMolecules(MoleculeType.Oxygen) > 0 Then
-
-                cell.InternalMolecules(MoleculeType.Glucose) -= 1
-                cell.InternalMolecules(MoleculeType.Oxygen) -= 1
-                cell.ATP = Math.Min(cell.ATP + 12, 1000)
-                AddMolecule(cell, MoleculeType.CarbonDioxide, 6)
-            End If
-        End If
-    End Sub
-End Class
 
 Public Class RuleScheduler
     Public Property Rules As List(Of IBiochemicalRule) = New List(Of IBiochemicalRule)
