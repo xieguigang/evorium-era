ef41d0aeefcb7978059eea9271e6f9b524788582
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-09 21:40:38 +0800

    snapshot data models

---
+++ src/EvoriumEra/Snapshot/SnapshotData/Snapshot.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic.Imaging
+
+Namespace Data
+
+    ''' <summary>
+    ''' A data snapshot of one time frame.
+    ''' </summary>
+    Public Class Snapshot
+        ' 元数据
+        Public Property Iteration As Long
+        Public Property Timestamp As DateTime
+        Public Property SimulationTime As TimeSpan
+
+        ' 环境快照
+        Public Property EnvironmentDimensions As SpatialIndex3D
+
+        ' 统计汇总
+        Public Property TotalLivingCells As Integer
+        Public Property TotalDeadCells As Integer
+        Public Property TotalMoleculesInSystem As Long
+        Public Property AverageATPCells As Double
+
+    End Class
+
+End Namespace
--- src/EvoriumEra/Snapshot/SnapshotFrame.vb
+++ src/EvoriumEra/Snapshot/SnapshotFrame.vb
@@ -1,26 +1,5 @@
-﻿Imports Microsoft.VisualBasic.Imaging
-
+﻿
 Namespace Data
-
-    ''' <summary>
-    ''' A data snapshot of one time frame.
-    ''' </summary>
-    Public Class Snapshot
-        ' 元数据
-        Public Property Iteration As Long
-        Public Property Timestamp As DateTime
-        Public Property SimulationTime As TimeSpan
-
-        ' 环境快照
-        Public Property EnvironmentDimensions As SpatialIndex3D
-
-        ' 统计汇总
-        Public Property TotalLivingCells As Integer
-        Public Property TotalDeadCells As Integer
-        Public Property TotalMoleculesInSystem As Long
-        Public Property AverageATPCells As Double
-
-    End Class
 
     Public Class SnapshotFrame
 
@@ -47,5 +26,4 @@ Namespace Data
             Return Nothing
         End Function
     End Class
-
 End Namespace
